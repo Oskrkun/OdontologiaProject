1cc249653679cef1e493813a2a4cdc06cde9b0bd
ZERK\palob <palobruno@gmail.com>
2017-03-11 15:57:23 -0300

    Commit inicial

---
+++ MyAgenda/app/src/main/java/com/myagenda/MainActivity.java
@@ -0,0 +1,35 @@
+package com.myagenda;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_pacientes);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_consultas);
+                    return true;
+            }
+            return false;
+        }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // mTextMessage = (TextView) findViewById(R.id.message);
+      //  BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+      //  navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
